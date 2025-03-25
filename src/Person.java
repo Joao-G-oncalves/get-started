b7f56d7ca3c7bb517240e868f9c7cb1a9a0c328c
Joao-G-oncalves <zaquir01@gmail.com>
2025-03-25 20:25:07 -0300

    last update of this lesson

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,32 @@
-public class Person {
-    public String name;
+import java.time.OffsetDateTime;
 
-    public int age;
+public class Person {
+    private final String name;
+
+    private int age;
+
+    private int lastYearAgeInc = OffsetDateTime.now().getYear();
+
+    public Person(String name){
+        this.name = name;
+        this.age = 1;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void incAge(){
+        if (this.lastYearAgeInc >= OffsetDateTime.now().getYear()) return;
+        this.age += 1;
+        this.lastYearAgeInc = OffsetDateTime.now().getYear();
+    }
 }
